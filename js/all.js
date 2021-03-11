bee259365f00d3e6c1d9a4e64503617a8c764668
Pickachu Chen <pickachu1006@gmail.com>
2021-03-11 11:01:55 +0800

    20210311_add alert box@ #contact

--- js/all.js
+++ js/all.js
@@ -20,21 +20,36 @@ $(document).ready(function () {
     });
 
     //偵測sns line btn 手機版還是電腦版，連結不同
-    var vw=$(window).width();
-    if (vw <= 768) {
-        $(".line_link").attr("href", "https://bit.ly/2YszXqD");
-    } else {
-        $(".line_link").attr("href", "https://line.me/R/ti/p/%40fhl1857j");
-    }
+    // var vw=$(window).width();
+    // if (vw <= 768) {
+    //     $(".line_link").attr("href", "https://bit.ly/2YszXqD");
+    // } else {
+    //     $(".line_link").attr("href", "https://line.me/R/ti/p/%40fhl1857j");
+    // }
     
-    //週年慶活動辦法下拉選單
-    // $(".anniversary_2 img,.anniversary_2, .SP_anniversary_2 img,.SP_anniversary_2").hide();
-    // $('.anniversary_1, .SP_anniversary_1').click(function (e) { 
-    //     e.preventDefault();
-    //     $(".anniversary_2 img,.anniversary_2, .SP_anniversary_2 img,.SP_anniversary_2").attr("style","display:block;");
-    // });
-    // $('.anniversary_2, .SP_anniversary_2').click(function (e) { 
-    //     e.preventDefault();
-    //     $(".anniversary_2 img,.anniversary_2, .SP_anniversary_2 img,.SP_anniversary_2").hide();
-    // });
+    //聊天機器人
+    $(window).scroll(function () {
+        var scrollPos = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        var formTop = $('#contact').position().top;
+        var width = $(window).width();
+
+        if ((formTop > (scrollPos + windowHeight / 2)) && (width <= 768)) {
+            $("#alert").attr({
+                style: "display: none;"
+            });
+        } else if ((formTop <= (scrollPos + windowHeight / 2)) && (width <= 768)) {
+            $("#alert").attr({
+                style: "display: block;"
+            });
+        } else if ((formTop > (scrollPos + windowHeight / 2)) && (width > 768)) {
+            $("#alert").attr({
+                style: "display: none;"
+            });
+        } else if ((formTop <= (scrollPos + windowHeight / 2)) && (width > 768)) {
+            $("#alert").attr({
+                style: "display: block;"
+            });
+        } 
+    });
 });
