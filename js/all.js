10b0bbc3604221c280f240ac0306e04e4f33dfbf
Pickachu Chen <pickachu1006@gmail.com>
2021-03-11 17:18:18 +0800

    20210311_update cart & tags

--- js/all.js
+++ js/all.js
@@ -19,19 +19,11 @@ $(document).ready(function () {
         },900);
     });
 
-    //偵測sns line btn 手機版還是電腦版，連結不同
-    // var vw=$(window).width();
-    // if (vw <= 768) {
-    //     $(".line_link").attr("href", "https://bit.ly/2YszXqD");
-    // } else {
-    //     $(".line_link").attr("href", "https://line.me/R/ti/p/%40fhl1857j");
-    // }
-    
     //聊天機器人
     $(window).scroll(function () {
         var scrollPos = $(window).scrollTop();
         var windowHeight = $(window).height();
-        var formTop = $('#contact').position().top;
+        var formTop = $('#acs_lp_form').position().top;
         var width = $(window).width();
 
         if ((formTop > (scrollPos + windowHeight / 2)) && (width <= 768)) {
